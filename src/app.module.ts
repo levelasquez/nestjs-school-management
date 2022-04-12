6fcbf762958f6d611e67b99cac6c44b4284646a7
Leonardo Velasquez <velasquezleonardo1@gmail.com>
2022-04-12 09:53:34 -0400

    Installing GraphQL Dependencies

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,14 @@
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
 
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      autoSchemaFile: true,
+      driver: ApolloDriver,
+    }),
+  ],
   controllers: [],
   providers: [],
 })
