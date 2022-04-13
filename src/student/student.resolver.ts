131d8819a21b219391cca6444fbec64306230c8f
Leonardo Velasquez <velasquezleonardo1@gmail.com>
2022-04-13 11:52:36 -0400

    Create Student Mutation

---
+++ src/student/student.resolver.ts
@@ -0,0 +1,16 @@
+import { Args, Mutation, Resolver } from '@nestjs/graphql';
+import { CreateStudentInput } from './create-student.input';
+import { StudentService } from './student.service';
+import { StudentType } from './student.type';
+
+@Resolver(() => StudentType)
+export class StudentResolver {
+  constructor(private studentService: StudentService) {}
+
+  @Mutation(() => StudentType)
+  createStudent(
+    @Args('createStudentInput') createStudentInput: CreateStudentInput,
+  ) {
+    return this.studentService.createStudent(createStudentInput);
+  }
+}
